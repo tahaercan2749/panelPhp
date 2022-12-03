75a3f19cf6358b993e93b7e2da32e3d0fd6acad3
Bülent ERCAN <tahaercan2749@gmail.com>
2022-12-03 22:44:07 +0300

    header menu eklendi eksiklikler duzenlendi maindiv yapıldı

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,13 @@
 const aside = document.querySelector('aside');
+const maindiv = document.querySelector('.main-div');
 const asideArrow = document.querySelector('.aside-arrow').addEventListener('click', function () {
 
     if (aside.classList.contains('small')) {
         aside.classList.remove('small');
+        maindiv.classList.remove('small');
     } else {
         aside.classList.add('small');
+        maindiv.classList.add('small');
     }
 });
 
